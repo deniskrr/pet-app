1ddf99b3513c5a7cb0799ed767d0c8effd8bd7a3
Denis Craciunescu <dennis.craciunescu@gmail.com>
2019-12-27 15:22:53 +0200

    Expand the search icon to a switch when that tab is selected

---
+++ lib/screens/home/search/pet_search_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class PetSearchPage extends StatefulWidget {
+  @override
+  _PetSearchPageState createState() => _PetSearchPageState();
+}
+
+class _PetSearchPageState extends State<PetSearchPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Text("Pet"),
+      ),
+    );
+  }
+}
