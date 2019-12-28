9c035ee71e8e5f1b994244d5494f8b1042e8a08f
Denis Craciunescu <dennis.craciunescu@gmail.com>
2019-12-28 14:54:30 +0200

    Add storage service

--- lib/services/services.dart
+++ lib/services/services.dart
@@ -1,6 +1,8 @@
 import 'package:get_it/get_it.dart';
 import 'package:pet_app/services/auth/auth_service.dart';
 import 'package:pet_app/services/auth/firebase_auth_service.dart';
+import 'package:pet_app/services/storage/firebase_storage_service.dart';
+import 'package:pet_app/services/storage/storage_service.dart';
 import 'package:pet_app/services/user/firebase_user_service.dart';
 import 'package:pet_app/services/user/user_service.dart';
 
@@ -9,4 +11,6 @@ final services = GetIt.instance;
 void initServices() {
   services.registerLazySingleton<UserService>(() => FirebaseUserService());
   services.registerLazySingleton<AuthService>(() => FirebaseAuthService());
+  services.registerLazySingleton<StorageService>(() =>
+      FirebaseStorageService());
 }
