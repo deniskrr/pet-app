7321e099307004f77cec2d4b29958c8c1e0ebe02
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-03 16:08:04 +0200

    Extract profile picture, input field and labeled checkbox to separate widgets

---
+++ lib/widgets/LabeledCheckbox.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/material.dart';
+
+class LabeledCheckbox extends StatelessWidget {
+  final String label;
+  final bool value;
+  final Function(bool) valueHandler;
+
+  const LabeledCheckbox({Key key, this.label, this.value, this.valueHandler})
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: <Widget>[
+        Text(
+          label,
+          style: TextStyle(fontSize: 16),
+        ),
+        Checkbox(
+          value: value,
+          onChanged: (bool val) => valueHandler(val),
+        )
+      ],
+    );
+  }
+}
