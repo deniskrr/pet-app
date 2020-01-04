871ed03bf7a2da216b652aa6e961b2b650afb534
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-04 14:55:56 +0200

    Add Get Petsitters method in User Service

--- lib/services/user/user_service.dart
+++ lib/services/user/user_service.dart
@@ -2,7 +2,10 @@ import 'package:pet_app/model/user.dart';
 
 abstract class UserService {
   Future<User> createUser(String email, String username, String uid);
+
   Future<User> getUser(String uid);
 
   Future<User> updateUser(User updatedUser);
+
+  Future<List<User>> getPetSitters();
 }
