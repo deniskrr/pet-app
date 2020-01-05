443631d8fcdfc845a37deb724617223d28e6dd8e
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-05 21:25:17 +0200

    Filter pets in service

--- lib/screens/home/search/pet/mating_search_results.dart
+++ lib/screens/home/search/pet/mating_search_results.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:pet_app/model/pet.dart';
-import 'package:pet_app/services/auth/auth_service.dart';
 import 'package:pet_app/services/pets/pets_service.dart';
 import 'package:pet_app/services/services.dart';
 import 'package:pet_app/widgets/pet_list_view.dart';
@@ -8,7 +7,6 @@ import 'package:pet_app/widgets/pet_list_view.dart';
 class MatingSearchResults extends StatelessWidget {
   final Pet forPet;
   final PetsService _petsService = services.get<PetsService>();
-  final AuthService _authService = services.get<AuthService>();
 
   MatingSearchResults({@required this.forPet});
 
@@ -20,8 +18,6 @@ class MatingSearchResults extends StatelessWidget {
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           List<Pet> pets = snapshot.data;
-          pets.removeWhere(
-              (Pet pet) => pet.ownerId == _authService.currentUserUid);
           return PetListView(
             petList: pets,
           );
