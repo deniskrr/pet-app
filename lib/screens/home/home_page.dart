e5b0a485eb64a9f6a9bac5a125cf8a603eae3a7d
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-12 11:36:29 +0200

    Change navigation from tabbar to bottomnav

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:pet_app/screens/home/chat/chats_overview_page.dart';
 import 'package:pet_app/screens/home/profile/profile_page.dart';
-import 'package:pet_app/screens/home/search/pet/pet_search_page.dart';
-import 'package:pet_app/screens/home/search/pet_sitter/pet_sitter_search_page.dart';
-import 'package:toggle_switch/toggle_switch.dart';
+import 'package:pet_app/screens/home/search/search_page.dart';
 
 class HomePage extends StatefulWidget {
   static final routeName = '/home';
@@ -12,79 +10,42 @@ class HomePage extends StatefulWidget {
   _HomePageState createState() => _HomePageState();
 }
 
-class _HomePageState extends State<HomePage>
-    with SingleTickerProviderStateMixin {
-  TabController _tabController;
-  int searchIndex = 0;
-
-  @override
-  void initState() {
-    _tabController = new TabController(vsync: this, length: 3);
-    super.initState();
-  }
-
-  @override
-  void dispose() {
-    _tabController.dispose();
-    super.dispose();
-  }
+class _HomePageState extends State<HomePage> {
+  int _currentIndex = 0;
+  final List<Widget> _children = [
+    ProfilePage(),
+    SearchPage(),
+    ChatsOverviewPage(),
+  ];
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Pet App"),
-        bottom: TabBar(
-          onTap: (index) {
-            setState(() {
-              //Needed to rebuild the TabBar (to make the search switch appear or disappear)
-            });
-          },
-          controller: _tabController,
-          tabs: <Widget>[
-            Tab(
-              icon: Icon(Icons.account_circle),
-            ),
-            Tab(
-              child: _tabController.index == 1
-                  ? ToggleSwitch(
-                minWidth: 50,
-                activeBgColor: Colors.transparent,
-                activeTextColor: Colors.white,
-                inactiveBgColor: Colors.transparent,
-                inactiveTextColor: Colors.white,
-                initialLabelIndex: searchIndex,
-                labels: ['', ''],
-                onToggle: (index) {
-                  _tabController.animateTo(1);
-                  setState(() {
-                    searchIndex = index;
-                  });
-                },
-                activeColors: [Colors.green, Colors.amberAccent],
-                icons: [
-                  Icons.pets,
-                  Icons.accessibility,
-                ],
-              )
-                  : Icon(Icons.search),
-            ),
-            Tab(
-              icon: Icon(Icons.chat),
-            )
-          ],
-        ),
-      ),
-      body: SafeArea(
-        child: TabBarView(
-          controller: _tabController,
-          children: <Widget>[
-            ProfilePage(),
-            searchIndex == 0 ? PetSearchPage() : PetSitterSearchPage(),
-            ChatsOverviewPage(),
-          ],
-        ),
+      body: _children[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        onTap: onTabTapped,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.account_circle),
+            title: Text("My Account"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search),
+            title: Text("Search"),
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.chat_bubble),
+            title: Text("Chats"),
+          ),
+        ],
       ),
     );
+  }
+
+  void onTabTapped(int index) {
+    setState(() {
+      _currentIndex = index;
+    });
   }
 }
