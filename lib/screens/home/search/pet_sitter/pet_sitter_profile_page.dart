b5fb4c711cfd9a4e91ff039a8aaeafe88b714146
bartc <bartcramba@gmail.com>
2020-01-17 20:53:57 +0200

    Adding a screen for the pet sitter profile

--- lib/screens/home/search/pet_sitter/pet_sitter_profile_page.dart
+++ lib/screens/home/search/pet_sitter/pet_sitter_profile_page.dart
@@ -1,98 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import '../../../../model/user.dart';
+import 'package:pet_app/model/user.dart';
 
-class PetSitterProfilePage extends StatefulWidget {
+class PetSitterProfilePage extends StatelessWidget {
   static final routeName = '/pet-sitter-profile';
 
   @override
-  State<StatefulWidget> createState() => _PetSitterProfilePage();
-}
-
-class _PetSitterProfilePage extends State<PetSitterProfilePage> {
-
-  @override
   Widget build(BuildContext context) {
-
     User currentUser = ModalRoute.of(context).settings.arguments;
 
-    return Scaffold (
+    return Scaffold(
         body: new Stack(
-          children: <Widget>[
-            ClipPath(
-              child: Container(color: Colors.black.withOpacity(0.8)),
-              clipper: getClipper(),
-            ),
-            Positioned(
-                width: 350.0,
-                top: MediaQuery.of(context).size.height / 5,
-                child: Column(
-                  children: <Widget>[
-                    Container(
-                        width: 150.0,
-                        height: 150.0,
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            image: DecorationImage(
-                                image: NetworkImage(currentUser.pictureUrl),
-                                fit: BoxFit.cover
-                            ),
-                            borderRadius: BorderRadius.all(Radius.circular(75.0)
-                            ),
-                            boxShadow: [
-                              BoxShadow(blurRadius: 7.0, color: Colors.black)
-                            ]
-                        )
-                    ),
-                    SizedBox(height: 90.0),
-                    Text(
-                      currentUser.username,
-                      style: TextStyle(
-                          fontSize: 30.0,
-                          fontWeight: FontWeight.bold,
-                          fontFamily: 'Montserrat'
-                      ),
-                    ),
-                    SizedBox(height: 15.0),
-                    Text(
-                      currentUser.bio,
-                      style: TextStyle(
-                        fontSize: 20.0,
-                        fontStyle: FontStyle.italic,
-                      ),
-                    ),
-                    SizedBox(height: 25.0),
-                    Container(
-                        height: 50.0,
-                        width: 95.0,
-                        child: Material(
-                            borderRadius: BorderRadius.circular(35.0),
-                            shadowColor: Colors.greenAccent,
-                            color: Colors.green,
-                            elevation: 7.0,
-                            child: GestureDetector(
-                                onTap: () {},
-                                child: Center(
-                                  child: Text(
-                                      'Start chat now',
-                                      style: TextStyle(
-                                          color: Colors.white)
-                                  ),
-                                )
-                            )
-                        )
-                    )
-                  ],
-                )
-            )
-          ],
-        )
-    );
+      children: <Widget>[
+        Positioned(
+            width: 350.0,
+            top: MediaQuery.of(context).size.height / 5,
+            child: Column(
+              children: <Widget>[
+                Container(
+                    width: 150.0,
+                    height: 150.0,
+                    decoration: BoxDecoration(
+                        color: Colors.red,
+                        image: DecorationImage(
+                            image: NetworkImage(currentUser.pictureUrl),
+                            fit: BoxFit.cover),
+                        borderRadius: BorderRadius.all(Radius.circular(75.0)),
+                        boxShadow: [
+                          BoxShadow(blurRadius: 7.0, color: Colors.black)
+                        ])),
+                SizedBox(height: 90.0),
+                Text(
+                  currentUser.username,
+                  style: TextStyle(
+                      fontSize: 30.0,
+                      fontWeight: FontWeight.bold,
+                      fontFamily: 'Montserrat'),
+                ),
+                SizedBox(height: 15.0),
+                Text(
+                  currentUser.bio,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                SizedBox(height: 25.0),
+                Container(
+                    height: 50.0,
+                    width: 95.0,
+                    child: FlatButton(
+                        padding: EdgeInsets.all(8.0),
+                        color: Colors.green,
+                        textColor: Colors.white,
+                        onPressed: () {},
+                        child: Align(
+                            alignment: Alignment.centerRight,
+                            child: Text(
+                              "Start chat now",
+                              style: TextStyle(fontSize: 20.0),
+                            ))))
+              ],
+            ))
+      ],
+    ));
   }
 }
 
-class getClipper extends CustomClipper<Path>{
-
+class getClipper extends CustomClipper<Path> {
   @override
   Path getClip(Size size) {
     var path = new Path();
@@ -108,5 +83,4 @@ class getClipper extends CustomClipper<Path>{
     // TODO: implement shouldReclip
     return true;
   }
-
 }
