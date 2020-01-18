87a1275ceaa83c915bd94f605d7546cd41757b20
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-18 20:36:56 +0200

    Fix chat user color

--- lib/widgets/chat_message_widget.dart
+++ lib/widgets/chat_message_widget.dart
@@ -35,9 +35,9 @@ class ChatMessageWidget extends StatelessWidget {
                 vertical: 10,
               ),
               decoration: BoxDecoration(
-                  color: chatMessage.sentByMe ?
-                  Theme.of(context).primaryColor :
-                  Colors.black26,
+                  color: chatMessage.sentByMe
+                      ? Theme.of(context).primaryColor
+                      : Colors.black26,
                   borderRadius: BorderRadius.circular(15)),
               child: Column(
                 mainAxisSize: MainAxisSize.min,
@@ -46,7 +46,7 @@ class ChatMessageWidget extends StatelessWidget {
                   if (!chatMessage.sentByMe)
                     Text(
                       correspondentName,
-                      style: TextStyle(color: Colors.greenAccent),
+                      style: TextStyle(color: Colors.black45),
                     ),
                   Text(
                     chatMessage.message,
