e372d6e67cb437e5a5c0a8811b379139af435b59
Denis Craciunescu <dennis.craciunescu@gmail.com>
2020-01-18 20:19:20 +0200

    Add correspondent name on chat tile

--- lib/widgets/chat_message_widget.dart
+++ lib/widgets/chat_message_widget.dart
@@ -3,24 +3,24 @@ import 'package:pet_app/model/chat_message.dart';
 
 class ChatMessageWidget extends StatelessWidget {
   final ChatMessage chatMessage;
+  final String correspondentName;
 
-  const ChatMessageWidget({
-    Key key,
-    this.chatMessage,
-  }) : super(key: key);
+  const ChatMessageWidget({Key key, this.chatMessage, this.correspondentName})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
       alignment:
-      chatMessage.sentByMe ? Alignment.centerRight : Alignment.centerLeft,
+          chatMessage.sentByMe ? Alignment.centerRight : Alignment.centerLeft,
       child: Row(
         mainAxisSize: MainAxisSize.min,
         children: <Widget>[
-          if (chatMessage.sentByMe) Expanded(
-            flex: 1,
-            child: Container(),
-          ), // make the message half the size of the row
+          if (chatMessage.sentByMe)
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ), // make the message half the size of the row
           Expanded(
             flex: 1,
             child: Container(
@@ -37,16 +37,28 @@ class ChatMessageWidget extends StatelessWidget {
               decoration: BoxDecoration(
                   color: chatMessage.sentByMe ? Colors.green : Colors.grey,
                   borderRadius: BorderRadius.circular(15)),
-              child: Text(
-                chatMessage.message,
-                style: TextStyle(color: Colors.white),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  if (!chatMessage.sentByMe)
+                    Text(
+                      correspondentName,
+                      style: TextStyle(color: Colors.greenAccent),
+                    ),
+                  Text(
+                    chatMessage.message,
+                    style: TextStyle(color: Colors.white),
+                  ),
+                ],
               ),
             ),
           ),
-          if (chatMessage.sentByMe == false) Expanded(
-            flex: 1,
-            child: Container(),
-          ), // make the message half the size of the row
+          if (chatMessage.sentByMe == false)
+            Expanded(
+              flex: 1,
+              child: Container(),
+            ), // make the message half the size of the row
         ],
       ),
     );
