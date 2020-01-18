366f460a030d18f9c1766154f1bce33389956c0a
Roxana Cristurean <roxana_cristurean@yahoo.com>
2020-01-18 21:22:38 +0200

    Empty pet list handle

--- lib/widgets/empty_list_container.dart
+++ lib/widgets/empty_list_container.dart
@@ -1,21 +1,31 @@
 import 'package:flutter/material.dart';
 
 class EmptyListContainer extends StatelessWidget {
-  final TextEditingController controller;
-  final String hintText;
-
-  const EmptyListContainer({Key key, this.controller, this.hintText}) : super(key: key);
+  const EmptyListContainer({Key key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: <Widget>[
-          Text(
-              'No data found',
+          Align(
+              child: Padding(
+                  padding: EdgeInsets.all(4.0),
+                  child: Text(
+                    "Your list is empty now!",
+                    style: TextStyle(fontSize: 16),
+                  ))),
+          SizedBox(
+            height: 10,
+          ),
+          Align(
+              child: Padding(
+            padding: EdgeInsets.all(4.0),
+            child: Text(
+              "Click + button to add new items!",
               style: TextStyle(fontSize: 16),
-          )
-        ]
-      );
-    }
+            ),
+          ))
+        ]);
+  }
 }
